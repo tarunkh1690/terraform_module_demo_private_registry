22febb314bd1b995c9e387c8c8ec73f713ac29e2
tarunkh1690 <51506748+tarunkh1690@users.noreply.github.com>
2023-09-13 20:58:37 +0530

    Update _remote_states.tf

--- _remote_states.tf
+++ _remote_states.tf
@@ -1,11 +1,11 @@
-data "terraform_remote_state" "terraform_module_demo_publicmodule" {
+data "terraform_remote_state" "terraform_module_demo_public_modules" {
     backend = "remote"
 
     config = {
         hostname = "app.terraform.io"
         organization = "tarun_org"
         workspaces = {
-          name = "terraform_module_demo_publicmodule"
+          name = "terraform_module_demo_public_modules"
         }
     }
 }
