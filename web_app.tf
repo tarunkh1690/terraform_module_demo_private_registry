96d677aea03e37fa3020043f86456477cacbe848
Tarun Khare <tarunkh1690@gmail.com>
2023-09-13 20:37:08 +0530

    added config

--- web_app.tf
+++ web_app.tf
@@ -59,32 +59,3 @@ module "web_application2" {
 
 
 }
-
-module "web_application3" {
-  source                 = "app.terraform.io/tarun_org/ec2-linux-inst/aws"
-  version                = "0.0.1-alpha"
-  ami                    = "ami-074dc0a6f6c764218"
-  client                 = "${var.client}"
-  tags                   = "${var.tags}"
-  instance_type          = "t2.micro"
-  key_name               = "keypair_${var.client.name}"
-  vpc_security_group_ids = ["${aws_security_group.webapp.id}"]
-  subnet_id              = "${local.aza_public_subnet_id}"
-  userDataScript         = base64encode(file(var.userDataScript))
- 
-
-
-  root_block_device = [
-    {
-      encrypted   = true
-      volume_size = "10"
-      volume_type = "gp2"
-    },
-  ]
-
-  host_prefix = "${var.client.prod_host_prefix}"
-
-  hosts = ["Web03"]
-
-
-}
